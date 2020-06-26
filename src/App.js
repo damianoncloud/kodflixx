a12ba01680869c65dd5d75d62957184887da2cea
Damiano Del Bello <dam6dam@gmail.com>
2020-06-26 16:53:23 +0200

    updated homepage with one movie cover

--- src/App.js
+++ src/App.js
@@ -6,22 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>
-          Welcome to Koflix
-        </h2>
-        <h4>Find your movie!</h4>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button>Register</button> 
-        </a>
-        <img src={favMovie} alt='favourite movie cover'/>
-      </header>
+      <h1>Welcome to Kodflix</h1>
+      <img src={favMovie} alt='fav-image' />
     </div>
   );
 }
