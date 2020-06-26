f5caf5613077356735e0dcda3acd133388b0768e
Damiano Del Bello <damianodelbello@iMac-di-Damiano.lan>
2020-06-26 10:17:05 +0200

    title modified

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h2>
           Welcome to Koflix
-        </p>
+        </h2>
+
         <a
           className="App-link"
           href="https://reactjs.org"
