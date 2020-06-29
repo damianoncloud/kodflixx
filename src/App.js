c5c2c51e7bc788df843a646e060312362fc5a215
Damiano Del Bello <dam6dam@gmail.com>
2020-06-29 18:27:06 +0200

    6 Tv show titles added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,21 @@ function App() {
     <div className="App">
       <h1>Welcome to Kodflix</h1>
       <img src={favMovie} alt='fav-image' />
+      
+      <div className="movie-container">
+        <div className="item">Black Diamonds</div>
+        <div className="item">Parasite</div>
+        <div className="item">Enemy of the State</div>
+      </div>  
+      <div className="movie-container">  
+        <div className="item">The Teory of everything</div>
+        <div className="item">Hitch</div>
+        <div className="item">Knives out</div>
+      </div>
+      
+
     </div>
   );
-}
+}   
 
 export default App;
