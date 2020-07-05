8d374ba1bf20640d1e72581962ebf1a5a0732268
Damiano Del Bello <dam6dam@gmail.com>
2020-07-05 18:08:00 +0200

    tv show title and animation

--- src/App.js
+++ src/App.js
@@ -16,24 +16,54 @@ function App() {
       <div className="movie-container">
         <div className="item">
           <img src={bloodDiamond} alt='blook-diamond'/>
+          <div class="overlay">
+            <h1 class="text">
+              Blood Diamond
+            </h1>
+          </div>
         </div>
         <div className="item">
           <img src={parasite} alt='parasite'/>
+          <div class="overlay">
+            <h1 class="text">
+              Parasite
+            </h1>
+          </div>
         </div>
         <div className="item">
           <img src={enemy} alt='enemy-of-state'/>
+          <div class="overlay">
+            <h1 class="text">
+              Enemy of the State 
+            </h1>
+          </div>
         </div>
       </div>  
 
       <div className="movie-container">  
         <div className="item">
           <img src={theory} alt='theory-of-everything'/>
+          <div class="overlay">
+            <h1 class="text">
+              The Theory of everything
+            </h1>
+          </div>
         </div>
         <div className="item">
           <img src={rocket} alt='rocketman'/>
+          <div class="overlay">
+            <h1 class="text">
+              Rocketman
+            </h1>
+          </div>
         </div>
         <div className="item">
           <img src={knives} alt='knives'/>
+          <div class="overlay">
+            <h1 class="text">
+              Knives out
+            </h1>
+          </div>
         </div>
       </div>
       
