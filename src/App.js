7fc808573aef64c6186c657aaca29f96f071e54a
Damiano Del Bello <dam6dam@gmail.com>
2020-07-11 18:49:21 +0200

    routing - navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Gallery from './Gallery.js';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Details from './Details.js';
 import './App.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Kodflix</h1>
-
-      <Gallery />
-
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Welcome to Kodflix</h1>
+        <Route exact path='/' component={Gallery} />
+        <Route exact path='/details' component={Details} />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
--- src/movieElement.js
+++ src/movieElement.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 
 
 export default function MovieElement(props) {
     return (
-      <div className="item">
+      <Link to='/details' className="item">
         <img src={props.image} alt={props.name}/>
         <div class="overlay">
           <h3 class="text">
             {props.name}
           </h3>
         </div>
-      </div>
+      </Link>
     );
 }
